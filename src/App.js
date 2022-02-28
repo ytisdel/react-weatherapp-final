2a4ec0525603ed414d49c867ac4436090fd33769
ytisdel <93150405+ytisdel@users.noreply.github.com>
2022-02-28 09:52:55 -0700

    cleaned unnecessary things and added link and heading to app.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+      <h1>React Weather App</h1>
+      <footer>
+        The app was made by Lona Tisdel and is{" "}
+        <a href="/" target="_blank" rel="noreferrer">
+          open-sourced
         </a>
-      </header>
+      </footer>
     </div>
   );
 }
