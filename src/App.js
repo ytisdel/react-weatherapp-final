f465866f722719c538a05e1447335346e2f9a9a8
ytisdel <93150405+ytisdel@users.noreply.github.com>
2022-02-28 10:22:57 -0700

    installed bootstrap

--- src/App.js
+++ src/App.js
@@ -3,17 +3,19 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>React Weather App</h1>
-      <footer>
-        The app was made by Lona Tisdel and is{" "}
-        <a
-          href="https://github.com/ytisdel/react-weatherapp-final"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced
-        </a>
-      </footer>
+      <div class="container">
+        <h1>React Weather App</h1>
+        <footer>
+          The app was made by Lona Tisdel and is{" "}
+          <a
+            href="https://github.com/ytisdel/react-weatherapp-final"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced.
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
