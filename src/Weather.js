6d956dd49d810c3af4265ffb1f7299c3f5cf555d
ytisdel <93150405+ytisdel@users.noreply.github.com>
2022-03-04 00:59:57 -0700

    Added new component FormattedDate to display the time and date on the weather app

--- src/Weather.js
+++ src/Weather.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import FormattedDate from "./FormattedDate";
 import axios from "axios";
 import "./Weather.css";
 
@@ -10,7 +11,7 @@ export default function Weather(props) {
       ready: true,
       temperature: response.data.main.temp,
       humidity: response.data.main.humidity,
-      date: "Thursday 02:00",
+      date: new Date(response.data.dt * 1000),
       description: response.data.weather[0].description,
       iconUrl: "https://ssl.gstatic.com/onebox/weather/64/sunny.png",
       wind: response.data.wind.speed,
@@ -42,7 +43,10 @@ export default function Weather(props) {
         </form>
         <h1 className="mt-3">{weatherData.city}</h1>
         <ul>
-          <li>{weatherData.date}</li>
+          <li>
+            {" "}
+            <FormattedDate date={weatherData.date} />
+          </li>
           <li>{weatherData.description}</li>
         </ul>
         <div className="row">
