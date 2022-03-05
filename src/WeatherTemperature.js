e6dc2403623bb5065ad4a1f92db274c0ca842696
ytisdel <93150405+ytisdel@users.noreply.github.com>
2022-03-05 12:19:34 -0700

    tracked the current unit of the WeatherTemperature component to display the temperature via the correct unit of measurement

--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -1,10 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function WeatherTemperature(props) {
-  return (
-    <span className="WeatherTemperature">
-      <span className="temperature">{Math.round(props.fahrenheit)}</span>
-      <span className="unit">°F</span>
-    </span>
-  );
+  const [unit, setUnit] = useState("fahrenheit");
+
+  function showFahrenheit(event) {
+    event.preventDefault();
+    setUnit("fahrenheit");
+  }
+
+  function showCelsius(event) {
+    event.preventDefault();
+    setUnit("celsius");
+  }
+
+  function celsius() {
+    return ((props.fahrenheit - 32) * 5) / 9;
+  }
+
+  if (unit === "fahrenheit") {
+    return (
+      <span className="WeatherTemperature">
+        <span className="temperature">{Math.round(props.fahrenheit)}</span>
+        <span className="unit">
+          °F |{" "}
+          <a href="/" onClick={showCelsius}>
+            °C
+          </a>
+        </span>
+      </span>
+    );
+  } else {
+    return (
+      <span className="WeatherTemperature">
+        <span className="temperature">{Math.round(celsius())}</span>
+        <span className="unit">
+          <a href="/" onCLick={showFahrenheit}>
+            °F
+          </a>{" "}
+          | °C
+        </span>
+      </span>
+    );
+  }
 }
