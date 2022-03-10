06508df5d95ef7452b430214cf8f42b4c3b71705
ytisdel <93150405+ytisdel@users.noreply.github.com>
2022-03-09 18:05:18 -0700

    Created a new component and started creating the layout for the weather forecast with a little styling added

---
+++ src/WeatherForecast.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./WeatherForecast.css";
+
+export default function WeatherForecast() {
+  return (
+    <div className="WeatherForecast">
+      <div className="row">
+        <div className="col">
+          <div className="WeatherForecast-day">Thu</div>
+          <img
+            src="http://openweathermap.org/img/wn/50n@2x.png"
+            alt="Sunny"
+            className="WeatherIcon"
+          />
+          <div className="WeatherForecast-temperatures">
+            <span className="WeatherForecast-temperature-max">19°</span>
+            <span className="WeatherForecast-temperature-min">10°</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
